6606e572e57bf068ed819b42887a283e8fab1581
Harshada1924 <bhongaleharshada918@gmail.com>
2026-09-10 14:04:39 +0530

    version 1.0.0

---
+++ pydemo.py
@@ -0,0 +1,22 @@
+#write a program to check if a number is odd or even
+#a=int(input("enter number ")) 
+#print("Number is Odd : ",a%2!=0)
+
+#write a program to print age in days
+#age=int(input("enter age "))
+#print(f"{a} years = {age*365} days" )
+
+#write a program to convert minutes into hours anage=int(input("enter age "))
+#min=int(input("enter minutes "))
+#print(f"{min} is {min//60} hours {min%60} minutes" )
+
+# write a program to extract the last digit of an number
+#number=int(input("enter number ")) 
+#print(f"{number} : last digit is {number%10}")
+
+#write a program to check if a person is eligible for discount the criteria must be a student and age must be below 21
+role=input("Enter Role(student/teacher) : ")
+age=int(input("Enter age : "))
+print(f"Eligible : {role=="student" and age<21} ") #harshada
+
+# write a program to swap two variables without a third variable, using arithmetic operations 
